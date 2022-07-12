e5084fb69462e38a4e93ab0594729d2dc9858228
Michael Parr <mparr5@asu.edu>
2022-07-12 17:51:34 -0400

    initial setup

---
+++ ProjectTemplate/Person.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ProjectTemplate
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int phoneNumber;
+    }
+}
